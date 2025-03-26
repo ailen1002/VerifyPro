5f2e998efc911dd50a6ead911b7afabc7b8a71dc
Andrew <ailen1002@163.com>
2025-03-26 08:20:19 +0800

    程序主页面布局完成

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,20 @@
 ﻿namespace VerifyPro.ViewModels;
+using ReactiveUI;
+using System.Reactive;
+using System;
 
-public class MainWindowViewModel : ViewModelBase
+public class MainWindowViewModel : ReactiveObject
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public ReactiveCommand<string, Unit> NavigateCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        NavigateCommand = ReactiveCommand.Create<string>(NavigateTo);
+    }
+
+    private void NavigateTo(string page)
+    {
+        Console.WriteLine($"导航到页面: {page}");
+        // 这里可以添加切换页面的逻辑，例如在 MainWindow 中替换 Content
+    }
 }
