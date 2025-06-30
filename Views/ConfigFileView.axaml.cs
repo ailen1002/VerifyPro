44faa6df397cb0907ea79fcc4350a6f51fb868e5
Andrew <ailen1002@163.com>
2025-06-30 10:00:43 +0800

    程序配置文件选择页面及功能实现

--- Views/ConfigFileView.axaml.cs
+++ Views/ConfigFileView.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace VerifyPro.Views;
@@ -9,5 +10,29 @@ public partial class ConfigFileView : Window
     public ConfigFileView()
     {
         InitializeComponent();
+    }
+    private async void OnSelectFileClick(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "选择配置文件",
+            Filters =
+            {
+                new FileDialogFilter { Name = "JSON 文件", Extensions = { "json" } },
+                new FileDialogFilter { Name = "所有文件", Extensions = { "*" } }
+            },
+            AllowMultiple = false
+        };
+
+        var result = await dialog.ShowAsync(this);
+        if (result != null && result.Length > 0)
+        {
+            SelectedFileNameTextBox.Text = result[0];
+        }
+    }
+
+    private void Close(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }
