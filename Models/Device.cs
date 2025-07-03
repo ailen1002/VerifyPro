9724f9a163dfd73b590a74b2bf1944ebeb1c4099
Andrew <ailen1002@163.com>
2025-07-03 16:01:24 +0800

    通讯设备Model定义

---
+++ Models/Device.cs
@@ -0,0 +1,17 @@
+﻿namespace VerifyPro.Models;
+
+public abstract class Device
+{
+    public class ModbusDevice
+    {
+        public required string Name { get; set; }
+        public required string Ip { get; set; }
+        public int Port { get; set; } = 502;
+    }
+    public class TestDevice
+    {
+        public required string Name { get; set; }
+        public required string Ip { get; set; }
+        public int Port { get; set; } = 9000;
+    }
+}
