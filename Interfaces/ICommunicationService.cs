db7f54813db911358ffac9e1139a5e11f33c1358
Andrew <ailen1002@163.com>
2025-07-03 16:01:01 +0800

    通讯接口定义

---
+++ Interfaces/ICommunicationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace VerifyPro.Interfaces;
+
+public interface ICommunicationService
+{
+    Task<bool> ConnectAsync(string ip, int port);
+    Task DisconnectAsync();
+    Task<byte[]> SendAsync(byte[] data);
+    bool IsConnected { get; }
+}
