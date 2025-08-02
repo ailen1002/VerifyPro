beb0a32de0aec60997c10e53c2bf7c5c1ca4bcb7
Andrew <ailen1002@163.com>
2025-08-02 08:20:35 +0800

    实现DetectLog自动滚动功能

--- Views/MainTestView.axaml.cs
+++ Views/MainTestView.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using ReactiveUI;
+using VerifyPro.ViewModels;
 
 namespace VerifyPro.Views;
 
@@ -7,5 +11,16 @@ public partial class MainTestView : Window
     public MainTestView()
     {
         InitializeComponent();
+        
+        this.WhenAnyValue(x => x.DataContext)!
+            .OfType<MainTestViewModel>()
+            .Select(vm => vm.WhenAnyValue(x => x.DetectLog))
+            .Switch()
+            .Throttle(TimeSpan.FromMilliseconds(100))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(_ =>
+            {
+                DetectLogScrollViewer?.ScrollToEnd();
+            });
     }
 }
